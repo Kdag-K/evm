da249d83caa05d234614ec659020279e5e2c8fdc
xiaoming <xiaoming@sensetime.com>
2020-08-04 16:33:03 +0800

    add common package and config package of evm

---
+++ src/common/types.go
@@ -0,0 +1,72 @@
+package common
+
+import (
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	ethTypes "github.com/ethereum/go-ethereum/core/types"
+)
+
+//Genesis File Structure
+type Genesis struct {
+	Alloc AccountMap
+	Poa   PoaMap
+}
+
+//AccountMap holds the alloc section of the genesis file
+type AccountMap map[string]struct {
+	Code        string            `json:"code"`
+	Storage     map[string]string `json:"storage"`
+	Balance     string            `json:"balance"`
+	Authorising bool              `json:"authorising"`
+	Nonce       uint64            `json:"nonce,omitempty"`
+}
+
+//PoaMap holds the poa section of the genesis file
+type PoaMap struct {
+	Address string
+	Balance string
+	Storage map[string]string
+	Abi     string
+	Code    string
+	Nonce   uint64 `json:"nonce,omitempty"`
+}
+
+//JSONReceipt is the JSON structure for the return receipt from the tx end
+//point
+type JSONReceipt struct {
+	Root              ethcommon.Hash     `json:"root"`
+	TransactionHash   ethcommon.Hash     `json:"transactionHash"`
+	From              ethcommon.Address  `json:"from"`
+	To                *ethcommon.Address `json:"to"`
+	GasUsed           uint64             `json:"gasUsed"`
+	CumulativeGasUsed uint64             `json:"cumulativeGasUsed"`
+	ContractAddress   ethcommon.Address  `json:"contractAddress"`
+	Logs              []*ethTypes.Log    `json:"logs"`
+	LogsBloom         ethTypes.Bloom     `json:"logsBloom"`
+	Status            uint64             `json:"status"`
+}
+
+// ToJSONReceipt uses a transaction, its from address, and a receipt to create
+// a JSONReceipt. The "from" addressed is derived from the transaction's
+// signature.
+func ToJSONReceipt(receipt *ethTypes.Receipt, tx *ethTypes.Transaction, signer ethTypes.Signer) *JSONReceipt {
+	from, _ := ethTypes.Sender(signer, tx)
+
+	jsonReceipt := JSONReceipt{
+		Root:              ethcommon.BytesToHash(receipt.PostState),
+		TransactionHash:   tx.Hash(),
+		From:              from,
+		To:                tx.To(),
+		GasUsed:           receipt.GasUsed,
+		CumulativeGasUsed: receipt.CumulativeGasUsed,
+		ContractAddress:   receipt.ContractAddress,
+		Logs:              receipt.Logs,
+		LogsBloom:         receipt.Bloom,
+		Status:            receipt.Status,
+	}
+
+	if receipt.Logs == nil {
+		jsonReceipt.Logs = []*ethTypes.Log{}
+	}
+
+	return &jsonReceipt
+}
---
+++ src/config/config.go
@@ -0,0 +1,143 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+var (
+	// Base
+	defaultLogLevel    = "debug"
+	defaultDataDir     = defaultHomeDir()
+	defaultEthAPIAddr  = ":8080"
+	defaultCache       = 128
+	defaultEthDir      = fmt.Sprintf("%s/eth", defaultDataDir)
+	defaultGenesisFile = fmt.Sprintf("%s/genesis.json", defaultEthDir)
+	defaultDbFile      = fmt.Sprintf("%s/chaindata", defaultEthDir)
+	defaultMinGasPrice = "0"
+)
+
+// Config contains de configuration for an EVM-Lite node
+type Config struct {
+	// Top-level directory of evm-babble data
+	DataDir string `mapstructure:"datadir"`
+
+	// Debug, info, warn, error, fatal, panic
+	LogLevel string `mapstructure:"log"`
+
+	// Genesis file
+	Genesis string `mapstructure:"genesis"`
+
+	// Location of ethereum account keys
+	Keystore string `mapstructure:"keystore"`
+
+	// File containing passwords to unlock ethereum accounts
+	PwdFile string `mapstructure:"pwd"`
+
+	// File containing the levelDB database
+	DbFile string `mapstructure:"db"`
+
+	// Address of HTTP API Service
+	EthAPIAddr string `mapstructure:"listen"`
+
+	// Megabytes of memory allocated to internal caching (min 16MB / database
+	// forced)
+	Cache int `mapstructure:"cache"`
+
+	// Minimum gasprice for transactions submitted through this node's service
+	MinGasPrice string `mapstructure:"min-gas-price"`
+
+	logger *logrus.Logger
+}
+
+// DefaultConfig returns the default configuration for an EVM-Lite node
+func DefaultConfig() *Config {
+	return &Config{
+		DataDir:     defaultDataDir,
+		LogLevel:    defaultLogLevel,
+		Genesis:     defaultGenesisFile,
+		DbFile:      defaultDbFile,
+		EthAPIAddr:  defaultEthAPIAddr,
+		Cache:       defaultCache,
+		MinGasPrice: defaultMinGasPrice,
+	}
+}
+
+// SetDataDir updates the root data directory and trickles down to the eth
+// directories if they are currently set to the default values.
+func (c *Config) SetDataDir(datadir string) {
+	c.DataDir = datadir
+
+	if c.Genesis == defaultGenesisFile {
+		c.Genesis = fmt.Sprintf("%s/eth/genesis.json", datadir)
+	}
+	if c.DbFile == defaultDbFile {
+		c.DbFile = fmt.Sprintf("%s/eth/chaindata", datadir)
+	}
+}
+
+// Logger returns a formatted logrus Entry that supports nested prefixes.
+func (c *Config) Logger() *logrus.Entry {
+	if c.logger == nil {
+		c.logger = logrus.New()
+		c.logger.Level = LogLevel(c.LogLevel)
+		c.logger.Formatter = new(prefixed.TextFormatter)
+	}
+	return c.logger.WithField("prefix", "evm-lite")
+}
+
+// LogLevel ...
+func LogLevel(l string) logrus.Level {
+	switch l {
+	case "debug":
+		return logrus.DebugLevel
+	case "info":
+		return logrus.InfoLevel
+	case "warn":
+		return logrus.WarnLevel
+	case "error":
+		return logrus.ErrorLevel
+	case "fatal":
+		return logrus.FatalLevel
+	case "panic":
+		return logrus.PanicLevel
+	default:
+		return logrus.DebugLevel
+	}
+}
+
+/*******************************************************************************
+FILE HELPERS
+*******************************************************************************/
+
+func defaultHomeDir() string {
+	// Try to place the data folder in the user's home dir
+	home := homeDir()
+	if home != "" {
+		if runtime.GOOS == "darwin" {
+			return filepath.Join(home, "Library", "EVMLITE")
+		} else if runtime.GOOS == "windows" {
+			return filepath.Join(home, "AppData", "Roaming", "EVMLITE")
+		} else {
+			return filepath.Join(home, ".evm-lite")
+		}
+	}
+	// As we cannot guess a stable location, return empty and handle later
+	return ""
+}
+
+func homeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	if usr, err := user.Current(); err == nil {
+		return usr.HomeDir
+	}
+	return ""
+}
