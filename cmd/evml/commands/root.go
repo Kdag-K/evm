c154f003ce4673d676cad61950ef5a3b58b3e348
bwhour <juvemeen.xio@gmail.com>
2021-07-27 14:41:10 +0800

    update some code

--- cmd/evml/commands/root.go
+++ cmd/evml/commands/root.go
@@ -5,16 +5,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//RootCmd is the root command for evml
-var RootCmd = &cobra.Command{
+// RootCmd is the root command for evml.
+var RootCmd = &cobra.Command{ //nolint:exhaustivestruct
 	Use:   "evml",
 	Short: "EVM",
 }
 
+//nolint:gochecknoinits
 func init() {
 	RootCmd.AddCommand(
 		run.RunCmd,
 	)
-	//do not print usage when error occurs
+	// do not print usage when error occurs.
 	RootCmd.SilenceUsage = true
 }
