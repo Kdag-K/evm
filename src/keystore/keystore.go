f671c14b0d793ecde637d8139dc4915f3a231e72
bwhour <juvemeen.xio@gmail.com>
2021-08-10 14:14:08 +0800

    add function Getkey

--- src/keystore/keystore.go
+++ src/keystore/keystore.go
@@ -7,21 +7,22 @@ import (
 	"path/filepath"
 
 	"github.com/Kdag-K/evm/src/config"
-
+	"github.com/Kdag-K/evm/src/crypto"
+	
 	// "github.com/Kdag-K/evm/src/crypto"
 	eth_crypto "github.com/ethereum/go-ethereum/crypto"
 )
 
-// GetKey looks in the keystore for a keyfile corresponding to the provided
-// moniker. If it exists, it decrypts it and returns the private key. Otherwise,
-// it returns an error
-// func GetKey(keystore, moniker, passwordFile string) (*ecdsa.PrivateKey, error) {
-// 	keyfile := filepath.Join(keystore, moniker+".json")
-//
-// 	// privateKey, err := crypto.GetPrivateKey(keyfile, passwordFile)
-//
-// 	return privateKey, nil
-// }
+GetKey looks in the keystore for a keyfile corresponding to the provided
+moniker. If it exists, it decrypts it and returns the private key. Otherwise,
+it returns an error
+func GetKey(keystore, moniker, passwordFile string) (*ecdsa.PrivateKey, error) {
+	keyfile := filepath.Join(keystore, moniker+".json")
+
+	privateKey, err := crypto.GetPrivateKey(keyfile, passwordFile)
+
+	return privateKey, nil
+}
 
 // DumpPrivKey converts an ecdsa private key into a hex string and writes it to
 // a file with UNIX permissions 600.
